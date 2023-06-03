2f38ed7e46133cfffabea17af7f4245c4c4ad879
EzlotheSage <bro19016@byui.edu>
2023-06-03 00:44:22 -0600

    a lot. added new lazyscripts lesson pages, added discover page to the chamber page. Yes, there are issues. I'm working on it. When I can.

--- chamber/scripts/discoverscript.js
+++ chamber/scripts/discoverscript.js
@@ -28,9 +28,9 @@ placeholderImages.forEach((image) => {
 });
 
 function calculateDaysBetweenDates(date1, date2) {
-  const oneDay = 24 * 60 * 60 * 1000;
-  const diffDays = Math.round(Math.abs((date1 - date2) / oneDay));
-  return diffDays;
+    const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
+    const diffDays = Math.round(Math.abs((date1 - date2) / oneDay));
+    return diffDays;
 }
 
 function displayDaysSinceLastVisit() {
