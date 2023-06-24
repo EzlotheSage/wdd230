ad357d915fe257426e683d532b877f1cd0979592
EzlotheSage <bro19016@byui.edu>
2023-06-24 00:19:00 -0600

    Created new windchill file, removed old one Added new OpenWeather API calls for weather section.

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,34 +1,49 @@
-// windchill.js
+const apiKey = 'b71b7c35ebce194fa2b43f67893af941';
+  
+  
+  const city = 'Boise, ID';
 
-// Function to calculate the wind chill factor
-function calculateWindChill(temperature, windSpeed) {
-    // Display current (sorta) temp and wind speed
-    document.getElementById("temperature").textContent = temperature.toFixed(1);
-    document.getElementById("windSpeed").textContent = windSpeed;
-    // Check if the temperature and wind speed meet the specification limits
-    if (temperature <= 50 && windSpeed > 3.0) {
-      // Calculate the wind chill factor using the formula
-      var windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
-  
-      // Display the wind chill factor value
-      console.log("Wind Chill Factor: " + windChill.toFixed(2) + "°F");
-  
-      // Update the page with the wind chill factor information
+  // Fetch weather data from OpenWeatherMap API
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=43.61&lon=-116.23&units=imperial&appid=bc7fb27d86655ce7753ba6f95336c886`)
+    .then(response => response.json())
+    .then(data => {
+      const temperatureElement = document.getElementById('temperature');
+      const windSpeedElement = document.getElementById('windSpeed');
+      const windChillElement = document.getElementById('windChill');
+      const weatherIconElement = document.getElementById('weatherIcon');
       
-      document.getElementById("windChill").textContent = windChill.toFixed(2) + "°F";
-    } else {
-      // Display "N/A" if the input values do not meet the requirements
-      console.log("N/A");
-  
-      // Update the page with the "N/A" message
-      document.getElementById("windChill").textContent = "N/A";
+      // Update temperature
+      temperatureElement.textContent = data.main.temp.toFixed(1);
+
+      // Update wind speed
+      windSpeedElement.textContent = data.wind.speed.toFixed(1);
+
+      // Calculate wind chill (if applicable)
+      const windChill = calculateWindChill(data.main.temp, data.wind.speed);
+      windChillElement.textContent = (windChill !== null) ? windChill.toFixed(1) : 'N/A';
+
+      // used for debugging, ignore
+      //console.log(temperatureElement);
+      //console.log(windSpeedElement);
+      //console.log(windChillElement);
+
+      const weatherIconCode = data.weather[0].icon;
+      const weatherIconUrl = 'http://openweathermap.org/img/wn/${weatherIconCode}.png';
+      weatherIconElement.src = weatherIconUrl;
+      weatherIconElement.alt = data.weather[0].description;
+    })
+
+
+
+    .catch(error => {
+      console.log('An error occurred while fetching weather data:', error);
+    });
+
+  // Function to calculate wind chill
+  function calculateWindChill(temperature, windSpeed) {
+    if (temperature <= 50 && windSpeed >= 3) {
+      const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+      return windChill;
     }
+    return null; // Wind chill not applicable
   }
-  
-  // Pre-defined temperature and wind speed values, sorta like May weather
-  var temperature = 70; 
-  var windSpeed = 5.0; 
-  
-  // Call the function to calculate and display the wind chill factor
-  calculateWindChill(temperature, windSpeed);
-  
