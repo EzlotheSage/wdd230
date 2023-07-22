8da0c906bf579361a4f00c578c897e4d73ff6bb4
EzlotheSage <bro19016@byui.edu>
2023-07-21 23:21:13 -0600

    test

--- BountifulFoods/scripts/drink.js
+++ BountifulFoods/scripts/drink.js
@@ -38,65 +38,68 @@ function populateSelectOptions() {
     });
   }
 
-document.getElementById('fruit-order-form').addEventListener('submit', function (event) {
-  event.preventDefault(); // Prevent the form from submitting normally
-
-  // Gather input values
-  const firstName = document.getElementById('first-name').value;
-  const email = document.getElementById('email').value;
-  const phone = document.getElementById('phone').value;
-  const fruit1 = document.getElementById('fruit-1').value;
-  const fruit2 = document.getElementById('fruit-2').value;
-  const fruit3 = document.getElementById('fruit-3').value;
-  const specialInstructions = document.getElementById('special-instructions').value;
-
-  // Get the order date
-  const orderDate = new Date().toLocaleDateString();
-
-  // Calculate the total nutrition based on selected fruits
-  const selectedFruits = [fruit1, fruit2, fruit3];
-  let totalNutrition = { carbohydrates: 0, protein: 0, fat: 0, sugar: 0, calories: 0 };
-
-  selectedFruits.forEach(fruit => {
-    if (nutritionData.hasOwnProperty(fruit)) {
-      totalNutrition.carbohydrates += nutritionData[fruit].carbohydrates;
-      totalNutrition.protein += nutritionData[fruit].protein;
-      totalNutrition.fat += nutritionData[fruit].fat;
-      totalNutrition.sugar += nutritionData[fruit].sugar;
-      totalNutrition.calories += nutritionData[fruit].calories;
-    }
+  document.getElementById('place-order-btn').addEventListener('click', function () {
+    // Gather input values
+    const firstName = document.getElementById('first-name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const fruit1 = document.getElementById('fruit-1').value;
+    const fruit2 = document.getElementById('fruit-2').value;
+    const fruit3 = document.getElementById('fruit-3').value;
+    const specialInstructions = document.getElementById('special-instructions').value;
+  
+    // Get the order date
+    const orderDate = new Date().toLocaleDateString();
+  
+    // Calculate the total nutrition based on selected fruits
+    const selectedFruits = [fruit1, fruit2, fruit3];
+    let totalNutrition = { carbohydrates: 0, protein: 0, fat: 0, sugar: 0, calories: 0 };
+  
+    selectedFruits.forEach(fruit => {
+      if (nutritionData.hasOwnProperty(fruit)) {
+        totalNutrition.carbohydrates += nutritionData[fruit].carbohydrates;
+        totalNutrition.protein += nutritionData[fruit].protein;
+        totalNutrition.fat += nutritionData[fruit].fat;
+        totalNutrition.sugar += nutritionData[fruit].sugar;
+        totalNutrition.calories += nutritionData[fruit].calories;
+      }
+    });
+  
+    // Format the output
+    const formattedOutput = `
+      <h2>Order Summary</h2>
+      <p><strong>Name:</strong> ${firstName}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Phone:</strong> ${phone}</p>
+      <p><strong>Order Date:</strong> ${orderDate}</p>
+      <p><strong>Selected Fruits:</strong> ${selectedFruits.join(', ')}</p>
+      <p><strong>Special Instructions:</strong> ${specialInstructions}</p>
+      <h3>Nutrition Information</h3>
+      <p><strong>Total Carbohydrates:</strong> ${totalNutrition.carbohydrates}g</p>
+      <p><strong>Total Protein:</strong> ${totalNutrition.protein}g</p>
+      <p><strong>Total Fat:</strong> ${totalNutrition.fat}g</p>
+      <p><strong>Total Sugar:</strong> ${totalNutrition.sugar}g</p>
+      <p><strong>Total Calories:</strong> ${totalNutrition.calories} calories</p>
+    `;
+  
+    // Display the formatted output in the modal
+    const modal = document.getElementById('modal');
+    const modalContent = modal.querySelector('.modal-content');
+    modalContent.innerHTML = formattedOutput;
+    modal.style.display = 'block';
+  
+    // Increment the form submission count and update it in local storage
+    formSubmissionCount++;
+    updateFormSubmissionCount(formSubmissionCount);
+  
+    // Update the form submission count on the page
+    document.getElementById('form-submission-count').textContent = formSubmissionCount;
+  
+    // Reset the form after submission
+    document.getElementById('fruit-order-form').reset();
   });
-
-  // Format the output
-  const formattedOutput = `
-    <h2>Order Summary</h2>
-    <p><strong>Name:</strong> ${firstName}</p>
-    <p><strong>Email:</strong> ${email}</p>
-    <p><strong>Phone:</strong> ${phone}</p>
-    <p><strong>Order Date:</strong> ${orderDate}</p>
-    <p><strong>Selected Fruits:</strong> ${selectedFruits.join(', ')}</p>
-    <p><strong>Special Instructions:</strong> ${specialInstructions}</p>
-    <h3>Nutrition Information</h3>
-    <p><strong>Total Carbohydrates:</strong> ${totalNutrition.carbohydrates}g</p>
-    <p><strong>Total Protein:</strong> ${totalNutrition.protein}g</p>
-    <p><strong>Total Fat:</strong> ${totalNutrition.fat}g</p>
-    <p><strong>Total Sugar:</strong> ${totalNutrition.sugar}g</p>
-    <p><strong>Total Calories:</strong> ${totalNutrition.calories} calories</p>
-  `;
-
-  formSubmissionCount++;
-  updateFormSubmissionCount(formSubmissionCount);
-
-  // Update the form submission count on the page
-  document.getElementById('form-submission-count').textContent = formSubmissionCount;
-
-  // Display the formatted output in the 'output' div
-  document.getElementById('output').innerHTML = formattedOutput;
-
-  // Open a new window with the nutrition information
-  const nutritionWindow = window.open('', '_blank');
-  nutritionWindow.document.write(formattedOutput);
-
-  // Reset the form after submission
-  document.getElementById('fruit-order-form').reset();
-});
+  
+  // Close the modal when the close button is clicked
+  document.getElementById('modal-close').addEventListener('click', function () {
+    document.getElementById('modal').style.display = 'none';
+  });
