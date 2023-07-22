cc91cf7f80f492ffb2ee814289a89fcc65369ed6
EzlotheSage <bro19016@byui.edu>
2023-07-22 00:46:58 -0600

    test

--- BountifulFoods/scripts/drink.js
+++ BountifulFoods/scripts/drink.js
@@ -10,117 +10,124 @@ document.addEventListener('DOMContentLoaded', function () {
         populateSelectOptions(); // Call a function to populate select options after fetching data
       })
       .catch(error => console.error('Error fetching data:', error));
-
-// Function to get the current form submission count from local storage
-function getFormSubmissionCount() {
-  const count = localStorage.getItem('formSubmissionCount');
-  return count ? parseInt(count) : 0;
-}
-
-// Function to update and store the form submission count in local storage
-function updateFormSubmissionCount(count) {
-  localStorage.setItem('formSubmissionCount', count);
-}
-
-function populateSelectOptions() {
-    // Get the select elements by ID
-    const fruitSelect1 = document.getElementById('fruit-1');
-    const fruitSelect2 = document.getElementById('fruit-2');
-    const fruitSelect3 = document.getElementById('fruit-3');
   
-    // Populate each select element with the available fruits
-    nutritionData.forEach(fruit => {
-      const option = document.createElement('option');
-      option.value = fruit.name; // Use the correct property name that represents the fruit's name
-      option.textContent = fruit.name; // Use the correct property name that represents the fruit's name
-      fruitSelect1.appendChild(option.cloneNode(true));
-      fruitSelect2.appendChild(option.cloneNode(true));
-      fruitSelect3.appendChild(option.cloneNode(true));
-    });
-  }
-
-  function displayModal(formattedOutput) {
-    // Display the formatted output in the modal
-    const modal = document.getElementById('modal');
-    const modalContent = modal.querySelector('.modal-content');
-
-    // Append the formatted output to the modal content
-    modalContent.innerHTML = formattedOutput;
-
-    // Show the modal
-    modal.style.display = 'block';
-  }
+    // Function to get the current form submission count from local storage
+    function getFormSubmissionCount() {
+      const count = localStorage.getItem('formSubmissionCount');
+      return count ? parseInt(count) : 0;
+    }
   
-  document.getElementById('place-order-button').addEventListener('click', function () {
-    // Gather input values
-    const firstName = document.getElementById('first-name').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const fruit1 = document.getElementById('fruit-1').value;
-    const fruit2 = document.getElementById('fruit-2').value;
-    const fruit3 = document.getElementById('fruit-3').value;
-    const specialInstructions = document.getElementById('special-instructions').value;
+    // Function to update and store the form submission count in local storage
+    function updateFormSubmissionCount(count) {
+      localStorage.setItem('formSubmissionCount', count);
+    }
   
-    // Get the order date
-    const orderDate = new Date().toLocaleDateString();
+    function populateSelectOptions() {
+      // Get the select elements by ID
+      const fruitSelect1 = document.getElementById('fruit-1');
+      const fruitSelect2 = document.getElementById('fruit-2');
+      const fruitSelect3 = document.getElementById('fruit-3');
   
-    // Calculate the total nutrition based on selected fruits
-    const selectedFruits = [fruit1, fruit2, fruit3];
-    let totalNutrition = { carbohydrates: 0, protein: 0, fat: 0, sugar: 0, calories: 0 };
+      // Populate each select element with the available fruits
+      nutritionData.forEach(fruit => {
+        const option = document.createElement('option');
+        option.value = fruit.name; // Use the correct property name that represents the fruit's name
+        option.textContent = fruit.name; // Use the correct property name that represents the fruit's name
+        fruitSelect1.appendChild(option.cloneNode(true));
+        fruitSelect2.appendChild(option.cloneNode(true));
+        fruitSelect3.appendChild(option.cloneNode(true));
+      });
+    }
   
-    selectedFruits.forEach(fruit => {
-      if (nutritionData.hasOwnProperty(fruit)) {
-        totalNutrition.carbohydrates += nutritionData[fruit].carbohydrates;
-        totalNutrition.protein += nutritionData[fruit].protein;
-        totalNutrition.fat += nutritionData[fruit].fat;
-        totalNutrition.sugar += nutritionData[fruit].sugar;
-        totalNutrition.calories += nutritionData[fruit].calories;
+    // Function to hide the modal
+    function hideModal() {
+      const modal = document.getElementById('modal');
+      modal.style.display = 'none';
+    }
+  
+    // Function to show the modal and set its content
+    function displayModal(formattedOutput) {
+      // Display the formatted output in the modal
+      const modal = document.getElementById('modal');
+      const modalContent = modal.querySelector('.modal-content');
+  
+      // Append the formatted output to the modal content
+      modalContent.innerHTML = formattedOutput;
+  
+      // Show the modal
+      modal.style.display = 'block';
+    }
+  
+    // Event listener to close the modal when clicking on the overlay
+    const modalOverlay = document.getElementById('modal');
+    modalOverlay.addEventListener('click', function (event) {
+      if (event.target === modalOverlay) {
+        hideModal();
       }
     });
   
-    // Format the output
-    const formattedOutput = `
-      <h2>Order Summary</h2>
-      <p><strong>Name:</strong> ${firstName}</p>
-      <p><strong>Email:</strong> ${email}</p>
-      <p><strong>Phone:</strong> ${phone}</p>
-      <p><strong>Order Date:</strong> ${orderDate}</p>
-      <p><strong>Selected Fruits:</strong> ${selectedFruits.join(', ')}</p>
-      <p><strong>Special Instructions:</strong> ${specialInstructions}</p>
-      <h3>Nutrition Information</h3>
-      <p><strong>Total Carbohydrates:</strong> ${totalNutrition.carbohydrates}g</p>
-      <p><strong>Total Protein:</strong> ${totalNutrition.protein}g</p>
-      <p><strong>Total Fat:</strong> ${totalNutrition.fat}g</p>
-      <p><strong>Total Sugar:</strong> ${totalNutrition.sugar}g</p>
-      <p><strong>Total Calories:</strong> ${totalNutrition.calories} calories</p>
-    `;
+    // Event listener to close the modal when the "Escape" key is pressed
+    document.addEventListener('keydown', function (event) {
+      if (event.key === 'Escape') {
+        hideModal();
+      }
+    });
   
-    // Display the formatted output in the modal
-    const modal = document.getElementById('modal');
-    const modalContent = modal.querySelector('.modal-content');
-    modalContent.innerHTML = formattedOutput;
-    modal.style.display = 'block';
+    document.getElementById('place-order-button').addEventListener('click', function () {
+      // Gather input values
+      const firstName = document.getElementById('first-name').value;
+      const email = document.getElementById('email').value;
+      const phone = document.getElementById('phone').value;
+      const fruit1 = document.getElementById('fruit-1').value;
+      const fruit2 = document.getElementById('fruit-2').value;
+      const fruit3 = document.getElementById('fruit-3').value;
+      const specialInstructions = document.getElementById('special-instructions').value;
+    
+      // Get the order date
+      const orderDate = new Date().toLocaleDateString();
+    
+      // Calculate the total nutrition based on selected fruits
+      const selectedFruits = [fruit1, fruit2, fruit3];
+      let totalNutrition = { carbohydrates: 0, protein: 0, fat: 0, sugar: 0, calories: 0 };
+    
+      selectedFruits.forEach(fruit => {
+        if (nutritionData.hasOwnProperty(fruit)) {
+          totalNutrition.carbohydrates += nutritionData[fruit].carbohydrates;
+          totalNutrition.protein += nutritionData[fruit].protein;
+          totalNutrition.fat += nutritionData[fruit].fat;
+          totalNutrition.sugar += nutritionData[fruit].sugar;
+          totalNutrition.calories += nutritionData[fruit].calories;
+        }
+      });
+    
+      // Format the output
+      const formattedOutput = `
+        <h2>Order Summary</h2>
+        <p><strong>Name:</strong> ${firstName}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone:</strong> ${phone}</p>
+        <p><strong>Order Date:</strong> ${orderDate}</p>
+        <p><strong>Selected Fruits:</strong> ${selectedFruits.join(', ')}</p>
+        <p><strong>Special Instructions:</strong> ${specialInstructions}</p>
+        <h3>Nutrition Information</h3>
+        <p><strong>Total Carbohydrates:</strong> ${totalNutrition.carbohydrates}g</p>
+        <p><strong>Total Protein:</strong> ${totalNutrition.protein}g</p>
+        <p><strong>Total Fat:</strong> ${totalNutrition.fat}g</p>
+        <p><strong>Total Sugar:</strong> ${totalNutrition.sugar}g</p>
+        <p><strong>Total Calories:</strong> ${totalNutrition.calories} calories</p>
+      `;
+    
+      displayModal(formattedOutput);
   
-    // Increment the form submission count and update it in local storage
-    formSubmissionCount++;
-    updateFormSubmissionCount(formSubmissionCount);
-  
-    // Update the form submission count on the page
-    document.getElementById('form-submission-count').textContent = formSubmissionCount;
-  
-    // Reset the form after submission
-    document.getElementById('fruit-order-form').reset();
+      // Increment the form submission count and update it in local storage
+      formSubmissionCount++;
+      updateFormSubmissionCount(formSubmissionCount);
+    
+      // Update the form submission count on the page
+      document.getElementById('form-submission-count').textContent = formSubmissionCount;
+    
+      // Reset the form after submission
+      document.getElementById('fruit-order-form').reset();
+    });
   });
   
-  // Close the modal when the close button is clicked
-  span.onclick(function) {
-    modal.style.display = "none";
-  }
-
-  window.onclick = function(event) {
-    if event.target == modal {
-        modal.style.display = "none";
-    }
-  }
-
-});
