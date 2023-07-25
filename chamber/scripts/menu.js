564a5ecf947765eb9318fc4e33beb583c3636aa8
EzlotheSage <bro19016@byui.edu>
2023-07-24 22:35:09 -0600

    added small media view for navbar

---
+++ chamber/scripts/menu.js
@@ -0,0 +1,21 @@
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarLinks = document.querySelector('.menu');
+
+navbarToggle.addEventListener('click', function () {
+navbarLinks.classList.toggle('active');
+});
+
+// Function to check if the small view media query is active
+function checkMediaQuery() {
+const mediaQuery = window.matchMedia('(max-width: 600px)');
+if (mediaQuery.matches) {
+    navbarLinks.classList.add('active');
+} else {
+    navbarLinks.classList.remove('active');
+}
+}
+
+// Check the media query on page load and whenever the window is resized
+window.addEventListener('load', checkMediaQuery);
+window.addEventListener('resize', checkMediaQuery);
+
