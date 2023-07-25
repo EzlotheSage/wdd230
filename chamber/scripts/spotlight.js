916c2f5928458d59383ca0b3ed454b3413e522e6
EzlotheSage <bro19016@byui.edu>
2023-07-24 22:13:58 -0600

    test

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -29,7 +29,7 @@ fetch('scripts/data.json')
             Phone: ${randomCompany1.phone}<br>
             Website: <a href="${randomCompany1.website}">${randomCompany1.website}</a><br>
             Membership Level: ${randomCompany1.membership_level}<br>
-            <img src="/images/${randomCompany1.image}" alt="${randomCompany1.name} Image">
+            <img src="./images/${randomCompany1.image}" alt="${randomCompany1.name} Image">
         `;
 
         const spotlight2 = document.querySelector('.spotlight2');
@@ -49,7 +49,7 @@ fetch('scripts/data.json')
             Phone: ${randomCompany3.phone}<br>
             Website: <a href="${randomCompany3.website}">${randomCompany3.website}</a><br>
             Membership Level: ${randomCompany3.membership_level}<br>
-            <img src="/images/${randomCompany3.image}" alt="${randomCompany3.name} Image">
+            <img src="images/${randomCompany3.image}" alt="${randomCompany3.name} Image">
         `;
     })
     .catch(error => {
