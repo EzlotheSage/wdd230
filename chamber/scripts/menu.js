00027f9edcd06a6cee4cafc260d1939f94cf6f93
EzlotheSage <bro19016@byui.edu>
2023-07-25 00:04:01 -0600

    test

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -6,6 +6,7 @@ function toggleMenu() {
   
   // Function to check if the small view media query is active
   function checkMediaQuery() {
+    const navbarLinks = document.querySelector('.menu');
     const mediaQuery = window.matchMedia('(max-width: 600px)');
     if (mediaQuery.matches) {
       toggleMenu();
