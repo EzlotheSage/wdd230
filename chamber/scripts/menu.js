58ede3257c3421b08aface38f3c011c8ceeff53e
EzlotheSage <bro19016@byui.edu>
2023-07-24 23:53:21 -0600

    test

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,26 +1,20 @@
-const navbarToggle = document.querySelector('.navbar-toggle');
-const navbarLinks = document.querySelector('.menu');
-
-navbarToggle.addEventListener('click', function () {
-navbarLinks.classList.toggle('active');
-});
-
+// Function to toggle the navigation menu
 function toggleMenu() {
-    const navbarLinks = document.querySelector('.menu ul');
+    const navbarLinks = document.querySelector('.menu');
     navbarLinks.classList.toggle('active');
   }
-
-// Function to check if the small view media query is active
-function checkMediaQuery() {
-const mediaQuery = window.matchMedia('(max-width: 600px)');
-if (mediaQuery.matches) {
-    navbarLinks.classList.add('active');
-} else {
-    navbarLinks.classList.remove('active');
-}
-}
-
-// Check the media query on page load and whenever the window is resized
-window.addEventListener('load', checkMediaQuery);
-window.addEventListener('resize', checkMediaQuery);
-
+  
+  // Function to check if the small view media query is active
+  function checkMediaQuery() {
+    const mediaQuery = window.matchMedia('(max-width: 600px)');
+    if (mediaQuery.matches) {
+      navbarLinks.classList.add('active');
+    } else {
+      navbarLinks.classList.remove('active');
+    }
+  }
+  
+  // Check the media query on page load and whenever the window is resized
+  window.addEventListener('load', checkMediaQuery);
+  window.addEventListener('resize', checkMediaQuery);
+  
