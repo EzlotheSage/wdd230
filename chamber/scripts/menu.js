8ec5f46089d02f44c7fe71f7ef09424027064df2
EzlotheSage <bro19016@byui.edu>
2023-07-25 19:27:31 -0600

    test

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,22 +1,36 @@
-// Function to toggle the navigation menu
-function toggleMenu() {
-    const navbarLinks = document.querySelector('.menu');
-    navbarLinks.classList.toggle('active');
+// Function to toggle the visibility of the menu items
+function toggleMenuItems() {
+  menuItems.classList.toggle("visible");
+}
+
+// Function to create the menu button and attach event listener
+function createMenuButton() {
+  // Check if screen width is less than or equal to 600 pixels
+  if (window.matchMedia("(max-width: 600px)").matches) {
+    const menuButton = document.createElement("button");
+    menuButton.textContent = "\u2630 Menu"; // Hamburger icon (Unicode character)
+    menuButton.id = "menu-button";
+    document.body.appendChild(menuButton);
+
+    // Attach click event listener to the menu button
+    menuButton.addEventListener("click", toggleMenuItems);
   }
-  
-  // Function to check if the small view media query is active
-  function checkMediaQuery() {
-    const navbarLinks = document.querySelector('.menu');
-    const mediaQuery = window.matchMedia('(max-width: 600px)');
-    if (mediaQuery.matches) {
-      toggleMenu();
-    } else {
-      const navbarLinks = document.querySelector('.menu');
-      navbarLinks.classList.remove('active');
-    }
+}
+
+// Function to check the screen width and show/hide the menu button
+function checkScreenWidth() {
+  const menuButton = document.getElementById("menu-button");
+  if (window.matchMedia("(max-width: 600px)").matches) {
+    menuButton.style.display = "block";
+  } else {
+    menuButton.style.display = "none";
+    menuItems.classList.remove("visible"); // Hide menu items if button is hidden
   }
-  
-  // Check the media query on page load and whenever the window is resized
-  window.addEventListener('load', checkMediaQuery);
-  window.addEventListener('resize', checkMediaQuery);
-  
+}
+
+// Initial check for screen width and create menu button if needed
+createMenuButton();
+checkScreenWidth();
+
+// Add event listener for screen width changes
+window.addEventListener("resize", checkScreenWidth);
