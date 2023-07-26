61410ef178a056bdfbbeaba36acb64f584f17b62
EzlotheSage <bro19016@byui.edu>
2023-07-25 22:40:50 -0600

    test

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,9 +1,9 @@
 const menuItems = document.getElementById("menu-items");
+const menuButton = document.getElementById("menu-button");
 let menuVisible = false; // Track the menu visibility state
 
 // Function to toggle the visibility of the menu items
 function toggleMenuItems() {
-  /* console.log('test') */
   if (!menuVisible) {
     // Show the menu items
     menuItems.style.maxHeight = menuItems.scrollHeight + "px";
@@ -14,27 +14,17 @@ function toggleMenuItems() {
   menuVisible = !menuVisible; // Toggle the menu visibility state
 }
 
-// Function to create the menu button and attach event listener
-function createMenuButton() {
-  const menuButton = document.createElement("button");
-  menuButton.textContent = "\u2630"; // Hamburger icon (Unicode character)
-  menuButton.id = "menu-button";
-  document.body.appendChild(menuButton);
-
-  // Attach click event listener to the menu button
-  menuButton.addEventListener("click", toggleMenuItems);
-}
+// Attach click event listener to the menu button
+menuButton.addEventListener("click", toggleMenuItems);
 
 // Function to check the screen width and show/hide the menu button
 function checkScreenWidth() {
-  const menuButton = document.getElementById("menu-button");
-
   if (window.matchMedia("(max-width: 600px)").matches) {
     menuButton.style.display = "block";
     menuItems.style.maxHeight = null; // Reset max-height when the screen size changes
   } else {
     menuButton.style.display = "none";
-    menuItems.style.maxHeight = "none"; // Show the menu items by default when screen size is larger
+    menuItems.style.maxHeight = "none"; // Show the menu items by default when the screen size is larger
   }
 }
 
@@ -43,6 +33,3 @@ checkScreenWidth();
 
 // Add event listener for screen width changes
 window.addEventListener("resize", checkScreenWidth);
-
-// Create menu button if needed
-createMenuButton();
