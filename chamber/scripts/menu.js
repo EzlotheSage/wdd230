d41c4c78199b25deda739738eb1db95cf45bf050
EzlotheSage <bro19016@byui.edu>
2023-07-25 19:42:56 -0600

    test

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -5,32 +5,32 @@ function toggleMenuItems() {
 
 // Function to create the menu button and attach event listener
 function createMenuButton() {
-  // Check if screen width is less than or equal to 600 pixels
-  if (window.matchMedia("(max-width: 600px)").matches) {
-    const menuButton = document.createElement("button");
-    menuButton.textContent = "\u2630 Menu"; // Hamburger icon (Unicode character)
-    menuButton.id = "menu-button";
-    document.body.appendChild(menuButton);
+  const menuButton = document.createElement("button");
+  menuButton.textContent = "\u2630 Menu"; // Hamburger icon (Unicode character)
+  menuButton.id = "menu-button";
+  document.body.appendChild(menuButton);
 
-    // Attach click event listener to the menu button
-    menuButton.addEventListener("click", toggleMenuItems);
-  }
+  // Attach click event listener to the menu button
+  menuButton.addEventListener("click", toggleMenuItems);
 }
 
 // Function to check the screen width and show/hide the menu button
 function checkScreenWidth() {
   const menuButton = document.getElementById("menu-button");
+  const menuItems = document.getElementById("menu-items");
+
   if (window.matchMedia("(max-width: 600px)").matches) {
     menuButton.style.display = "block";
+    menuItems.classList.remove("visible"); // Hide menu items if button is shown
   } else {
     menuButton.style.display = "none";
-    menuItems.classList.remove("visible"); // Hide menu items if button is hidden
+    menuItems.classList.add("visible"); // Show menu items if button is hidden
   }
 }
 
 // Initial check for screen width and create menu button if needed
-createMenuButton();
 checkScreenWidth();
+createMenuButton();
 
 // Add event listener for screen width changes
 window.addEventListener("resize", checkScreenWidth);
